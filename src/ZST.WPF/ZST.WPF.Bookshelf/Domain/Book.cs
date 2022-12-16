d50caf9e36d4c4e842d90a1eb149042ea560357d
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-12-16 16:33:28 +0100

    Wyświetlanie listy książek

---
+++ src/ZST.WPF/ZST.WPF.Bookshelf/Domain/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZST.WPF.Bookshelf.Domain
+{
+    public abstract class Base 
+    {
+        public int Id { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        
+    }
+
+    public class Book : Base
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public int PublishedYear { get; set; }
+        public decimal Price { get; set; }
+        public string ISBN { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return $"\"{Title}\" {Author} {Description}";
+            }
+        }
+
+        public override string ToString()
+        {
+            return FullName;
+        }
+
+        public Book(string title, string author, string description, int publishedYear, decimal price, string iSBN)
+        {
+            Title = title;
+            Author = author;
+            Description = description;
+            PublishedYear = publishedYear;
+            Price = price;
+            ISBN = iSBN;
+        }
+    }
+}
---
+++ src/ZST.WPF/ZST.WPF.Bookshelf/ViewModels/BooksViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZST.WPF.Bookshelf.Domain;
+
+namespace ZST.WPF.Bookshelf.ViewModels
+{
+    public class BooksViewModel
+    {
+        public ObservableCollection<Book> Books { get; set; }
+
+        public BooksViewModel()
+        {
+            Books = new ObservableCollection<Book>();
+
+            Books.Add(new Book("WPF dla Opornych", "Marcin", "Lorem ipsum...", 2022, 99, "ABC123"));
+
+            Books.Add(new Book("SQL dla Opornych", "Marek", "Lorem ipsum...", 2020, 199, "XYZ123"));
+
+            Books.Add(new Book("C dla Opornych", "Edyta", "Lorem ipsum...", 2022, 299, "AAA123"));
+
+
+        }
+
+
+    }
+}
